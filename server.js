fda87176169dcc5657915210aa28706a8b4593a1
Joshua Bidwell <jjbidwell262@gmail.com>
2021-10-01 14:49:35 -0700

    added home route

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.sendFile("../index.html");
-});
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "index.html"))
+);
 
 app.listen(3001, () => console.log(`server listening on port 3001`));
