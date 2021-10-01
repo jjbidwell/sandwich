1d2b296d1932a45152104235d893d03027e2d767
Joshua Bidwell <jjbidwell262@gmail.com>
2021-10-01 14:47:26 -0700

    moved index to public and started routing

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile("../index.html");
+});
+
+app.listen(3001, () => console.log(`server listening on port 3001`));
